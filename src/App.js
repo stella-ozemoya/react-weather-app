3aedc77873ce34f71e3c3daaa3c3e6e9a0db2501
stella-ozemoya <167516349+stella-ozemoya@users.noreply.github.com>
2024-07-16 08:44:18 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <div className="container" >
+      <h1>Weather App</h1>
+      <footer>
+        This Project was coded by {""}
+        This project was coded by {" "}
+      <a href="https://weather-forecast-47ew.vercel.app/" target="_blank">
+      Stella Ozemoya
+      </a> {" "}
+      and is {" "} 
+
+      <a 
+      href="https://github.com/stella-ozemoya/my-react-app" 
+      target="_blank">
+
+        open-sourced on GitHub
+        </a>
+      </footer>
+      </div>
+      </div>
   );
 }
 
